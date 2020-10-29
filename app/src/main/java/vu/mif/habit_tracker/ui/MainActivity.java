e96885935d038a7d23fb1099ec28c04c809174d1
Metr0xFF <36664298+Metr0xFF@users.noreply.github.com>
2020-10-29 15:06:09 +0200

    Initial project commit

---
+++ app/src/main/java/vu/mif/habit_tracker/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package vu.mif.habit_tracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vu.mif.habit_tracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
